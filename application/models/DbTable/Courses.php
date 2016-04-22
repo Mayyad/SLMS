30a41fbec5a33e7b19675d8dcb0d0da94ec7ddaf
Mohamed ayyad <moh.ayad93@hotmail.com>
2016-04-22 20:25:44 +0200

    cateogry and course fnshied

--- application/models/DbTable/Courses.php
+++ application/models/DbTable/Courses.php
@@ -22,6 +22,10 @@ class Application_Model_DbTable_Courses extends Zend_Db_Table_Abstract
         if (isset($data['action'])){
             unset ($data['action']);
         }
+        
+        if (isset($data['MAX_FILE_SIZE'])){
+            unset ($data['MAX_FILE_SIZE']);
+        }
 
         $data['course_status'] = '1' ;
 
@@ -47,6 +51,38 @@ class Application_Model_DbTable_Courses extends Zend_Db_Table_Abstract
         $this->delete($where);
     }
     
+    function returnCourse($id){
+
+        
+        return $this->find($id)->toArray();
+    }
+    
+    function updateCourse($data , $id) {
+        if (isset($data['module'])){
+            unset ($data['module']);
+        }
+        if (isset($data['controller'])){
+            unset ($data['controller']);
+        }
+        if (isset($data['action'])){
+            unset ($data['action']);
+        }
+        
+        if (isset($data['MAX_FILE_SIZE'])){
+            unset ($data['MAX_FILE_SIZE']);
+        }
+        
+         if (isset($data['submit'])){
+            unset ($data['submit']);
+        }
+        
+        if (isset($data['id'])){
+            unset ($data['id']);
+        }
+        
+        $this->update($data, "course_id= $id");
+                        
+    }
     
 
 }
