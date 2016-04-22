ca31d4c075fbd84a2fd804421e34d335da023c15
Mohamed ayyad <moh.ayad93@hotmail.com>
2016-04-22 15:17:14 +0200

    compelte course

--- application/forms/AddCourse.php
+++ application/forms/AddCourse.php
@@ -6,30 +6,36 @@ class Application_Form_AddCourse extends Zend_Form
     public function init()
     {
         /* Form Elements & Other Definitions Here ... */
-        $course_name = new Zend_Form_Element_Text("courseName");
+        $course_name = new Zend_Form_Element_Text("course_name");
         $course_name->setRequired();
         $course_name->addValidator(new Zend_Validate_Alpha());
         $course_name->setlabel("Course Name:");
         $course_name->setAttrib("class",array("form-control","col-lg-9" ));
         $course_name->setAttrib("placeholder","Enter Course name");
         
-        $course_desc = new Zend_Form_Element_Text("courseName");
+        $course_desc = new Zend_Form_Element_Text("course_desc");
         $course_desc->setRequired();
         $course_desc->addValidator(new Zend_Validate_Alpha());
         $course_desc->setlabel("Course Descrption:");
         $course_desc->setAttrib("class",array("form-control","col-lg-9" ));
         $course_desc->setAttrib("placeholder","Enter Course desc");
         
-        $course_photo=new Zend_Form_Element_File('photo');
+        $course_status = new Zend_Form_Element_Hidden("course_status");
+        $course_status->setAttrib("value", "Available");
+        
+        $course_photo=new Zend_Form_Element_File('course_photo');
         $course_photo->setLabel("Course Cover : ");
         $course_photo->setAttrib("class",array("form-control","col-lg-9" ));
-        $course_photo->setAttrib('id','image-file');
-
+        $course_photo->setAttrib('id','course-img');
+        
+       
         $submit = new Zend_Form_Element_Submit('submit');
         $submit->setAttrib("class","btn-lg btn-primary");
+        $submit->setAttrib("value", "add");
         $this->setAttrib('enctype', 'multipart/form-data');
         $this->setAttrib('class','form-horizontal');
-        $this->addElements(array($course_name , $course_photo));
+        
+        $this->addElements(array($course_name , $course_desc , $course_status , $course_photo, $submit ));
     }
 
 
--- application/models/DbTable/Courses.php
+++ application/models/DbTable/Courses.php
@@ -23,7 +23,11 @@ class Application_Model_DbTable_Courses extends Zend_Db_Table_Abstract
             unset ($data['action']);
         }
         
-        $data['course_status'] = '1' ;
+        if (isset($data['MAX_FILE_SIZE'])){
+            unset ($data['MAX_FILE_SIZE']);
+        }
+        
+        $data['course_status'] = "Available";
     	
     	$this->insert($data);
     }
@@ -40,11 +44,44 @@ class Application_Model_DbTable_Courses extends Zend_Db_Table_Abstract
             unset ($data['action']);
         }
         
-        
- 
         $where = $this->getAdapter()->quoteInto('course_id = ?', $id['id']);
         
         $this->delete($where);
+    }
+    
+    function returnCourse($id){
+
+        
+        return $this->find($id)->toArray();
+    }
+    
+   
+    
+    function updateCourse($data , $id) {
+        if (isset($data['module'])){
+            unset ($data['module']);
+        }
+        if (isset($data['controller'])){
+            unset ($data['controller']);
+        }
+        if (isset($data['action'])){
+            unset ($data['action']);
+        }
+        
+        if (isset($data['MAX_FILE_SIZE'])){
+            unset ($data['MAX_FILE_SIZE']);
+        }
+        
+         if (isset($data['submit'])){
+            unset ($data['submit']);
+        }
+        
+        if (isset($data['id'])){
+            unset ($data['id']);
+        }
+        
+        $this->update($data, "course_id= $id");
+                        
     }
 
 }
